03ba4455ea082a382f8a44dbde864139cef4fb16
joaquin-gopick <140534244+joakinCD@users.noreply.github.com>
2023-11-29 11:01:08 +0100

    dev commit

--- src/components/CustomItem.js
+++ src/components/CustomItem.js
@@ -1,19 +1,115 @@
 import Image from 'next/image';
+import { useState} from "react";
+import Link from 'next/link'
 const CustomItem = (props) => {
-    console.log("props",props)
-    let pelicula = props.pelicula
+    const [isHovering, setIsHovered] = useState(false);
+    const onMouseEnter = () => {
+        setIsHovered(true)
+    };
+    const onMouseLeave = () => setIsHovered(false);
+    const eliminar=() =>{
+        if(window.confirm("¿Seguro que quieres eliminar este elemento?")){
+            props.eliminarElemento()
+        }
+    }
+    let elemento = props.multimedia
+    let urlIcon = '/icons/film-icon.svg'
+    switch(elemento.type){
+        case 'pelicula':
+            urlIcon = '/icons/film-icon.svg'
+        break;
+        case 'ebook':
+            urlIcon = '/icons/ebook-icon.svg'
+        break;
+        case 'videojuego':
+            urlIcon = '/icons/videojuego-icon.svg'
+        break;
+    }
     return(
-        <li style={{borderStyle:'solid', borderWidth:1,borderRadius:8,padding:8,margin:8}}>
-            <div style={{width: '100%', height:200, position:'relative'}}>
+        <li 
+            style={{margin:8}} 
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}>
+            <div style={{overflow:'hidden',width: 250,height:250,borderStyle:'solid', borderWidth:1,borderRadius:8,borderColor:'black',}}>
               <Image
-                alt='Mountains'
                 src='/images/pelicula.png'
-                layout='fill'
+                width={250}
+                height={250}
+                style={{borderRadius:8}}
+                alt='Foto'
+                priority
               />
             </div>
-            <div>
-                <a style={{fontWeight:'bold'}}>{pelicula.nombre}</a>
+            <div style={{padding:8,borderRadius:8,height:60,width:250,transform:'translateY(-61px)',position:'absolute',background: 'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)',display:'flex',flexDirection:'row'}}>
+                <div style={{display:'flex',flexDirection:'column',flex:1}}>
+                    <div style={{display:'flex'}}>
+                        <Image
+                            width={22}
+                            height={22}
+                            src={urlIcon}
+                            style={{ filter: 'invert(100%)'}}
+                            alt='Icono'
+                        />
+                        <a style={{fontWeight:'bold',marginLeft:4,color:'white'}}>{elemento.nombre}</a>
+                    </div>
+                    <div style={{display:'flex'}}>
+                        <Image
+                            width={22}
+                            height={22}
+                            src={'/icons/fecha-icon.svg'}
+                            style={{ filter: 'invert(100%)'}}
+                            alt='Icono fecha'
+                        />
+                        <a style={{marginLeft:4,color:'white'}}>{elemento.fecha}</a>
+                    </div>
+                </div>
+                <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:50}}>
+                    <div style={{background:'grey',width:40,height:40,borderRadius:8,display:'flex',justifyContent:'center',alignItems:'center'}}>
+                        <a style={{fontWeight:'bold',color:'white',fontSize:20}}>{elemento.puntuacion}</a>
+                    </div>
+                </div>
             </div>
+            {isHovering?
+                (
+                    <div style={{borderRadius:8,opacity:0.9,position:'absolute',background:'black',width:250,height:250,transform:'translateY(-250px)'}}>
+                        <div style={{margin:8,display:'flex', flexDirection:'column',height:'calc(100% - 16px)'}}>
+                            <div style={{display:'flex'}}>
+                                    <Image
+                                        width={22}
+                                        height={22}
+                                        src={urlIcon}
+                                        style={{ filter: 'invert(100%)'}}
+                                        alt='Icono'
+                                    />
+                                    <a style={{fontWeight:'bold',marginLeft:4,color:'white'}}>{elemento.nombre}</a>
+                                </div>
+                            <a style={{color:'white',flex:1}}>{elemento.resumen}</a>
+                            <div style={{height:50,display:'flex',justifyContent:'flex-end',alignItems:'center'}}>
+                                <Link href={"/formulario/"+elemento.id} style={{marginRight:8,background:'grey',width:40,height:40,borderRadius:8,display:'flex',justifyContent:'center',alignItems:'center'}}>
+                                    <Image
+                                        width={22}
+                                        height={22}
+                                        src={'/icons/editar-icon.svg'}
+                                        style={{ filter: 'invert(100%)'}}
+                                        alt='Icono editar'
+                                    />
+                                </Link>
+                                <button onClick={eliminar} style={{background:'grey',width:40,height:40,borderRadius:8,display:'flex',justifyContent:'center',alignItems:'center'}}>
+                                    <Image
+                                        width={22}
+                                        height={22}
+                                        src={'/icons/eliminar-icon.svg'}
+                                        style={{ filter: 'invert(100%)'}}
+                                        alt='Icono eliminar'
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )
+                :
+                (null)
+            }
         </li>
     );
 }
---
+++ src/pages/formulario/[id]/index.js
@@ -0,0 +1,139 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Formulario.module.css'
+import React, { useState,useEffect } from "react"
+import Cabecera from '../../../components/Cabecera'
+import {useRouter} from 'next/navigation'
+const inter = Inter({ subsets: ['latin'] })
+
+import { useParams,useSearchParams } from 'next/navigation'
+
+export default function Page() {
+  const params = useParams()
+   const router = useRouter()
+
+ 
+  const [elementoSel, setElementoSel] = useState({});
+  const [selectValue, setSelectValue] = useState('pelicula');
+  const [indiceElemento, setIndiceElemento] = useState(-1);
+  const [listadoMultimedia, setListadoMultimedia] = useState([]);
+  const [formData, setFormData] = useState({
+    nombre: "",
+    resumen: "",
+    fecha:""
+  });
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+  
+  const handleSelect = (e) => {
+    setSelectValue(e.target.value);
+  }
+  const editarElemento = () => {
+    if(formData.nombre.length>0){
+      if(formData.resumen.length>0){
+        if(formData.fecha.length>0){
+          const elementoCopia = JSON.parse(JSON.stringify(elementoSel))
+          elementoCopia.nombre=formData.nombre
+          elementoCopia.resumen=formData.resumen
+          elementoCopia.fecha=formData.fecha
+          elementoCopia.type=selectValue
+          listadoMultimedia[indiceElemento]=elementoCopia
+          localStorage.setItem('listadoMultimedia',JSON.stringify(listadoMultimedia))
+          alert('Elemento editado correctamente')
+          router.replace('/')  
+        }else{
+          alert('Introduzca una fecha')
+        }
+      }else{
+        alert('Introduzca un resumen')
+      }
+    }else{
+      alert('Introduzca un título')
+    }
+    
+  }
+  
+  useEffect(() => {
+    let listadoMultimediaAux = JSON.parse(localStorage.getItem('listadoMultimedia'))
+
+    setListadoMultimedia(listadoMultimediaAux)
+    if(listadoMultimediaAux?.length>0){
+      listadoMultimediaAux.map(function(item,index){
+        if(params?.id==item?.id?.toString()){
+          setElementoSel(item)
+          setFormData({
+            nombre: item.nombre,
+            resumen: item.resumen,
+            fecha:item.fecha
+          })
+          setSelectValue(item.type)
+          setIndiceElemento(index)
+          }
+      })
+    }
+  }, [params])
+ 
+  return (
+    <>
+      <Head>
+        <title>Editar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+
+        <div style={{width:'100%',height:'100%'}}>
+          <Cabecera nombre='Formulario'></Cabecera>
+          {elementoSel.id?(
+            <div style={{display:'flex', margin:10, alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+              <p style={{fontSize:24,fontWeight:'bold',color:'black',marginTop:30,marginBottom:30}}>Editar elemento</p>
+              <form className={`${styles.form}`}>
+                
+                <div>
+                  <label>Tipo</label>
+                  <select name="select" onChange={handleSelect}>
+                    <option selected={selectValue=='pelicula'} value="pelicula">Pelicula</option>
+                    <option selected={selectValue=='videojuego'} value="videojuego" >Videojuego</option>
+                    <option selected={selectValue=='ebook'} value="ebook">Ebook</option>
+                  </select>
+                </div>
+                <div >
+                  <label>Título</label>
+                  <input type="text" placeholder="Título" name="nombre" onChange={handleInput} value={formData.nombre} />
+                </div>
+                
+                <div>
+                  <label>Fecha</label>
+                  <input type="date" name="fecha" onChange={handleInput} value={formData.fecha}/>
+                </div>
+                <div>
+                  <label>Resumen</label>
+                  <textarea type="text" placeholder="Resumen" name="resumen" onChange={handleInput} value={formData.resumen} />
+                </div>
+                
+              
+
+                
+              </form>
+              <button  className={`${styles.button}`} onClick={editarElemento}>Editar</button>
+          </div>)
+          :(
+            <div style={{display:'flex', margin:10, alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+              <p style={{fontSize:24,fontWeight:'bold',color:'black',marginTop:30,marginBottom:30}}>No existe ese elemento</p>
+            </div>
+          )}
+          
+        </div>
+      </main>
+    </>
+    
+  )
+}
